207506142d1e7784c87d5715a4ae8cae82ac75c6
Jeong Dong <eejxdong99@gmail.com>
2023-11-16 21:07:05 +0900

    N: init project

---
+++ VoiceMemoApp/VoiceMemoApp/VoiceMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoAppApp.swift
+//  VoiceMemoApp
+//
+//  Created by 이정동 on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
