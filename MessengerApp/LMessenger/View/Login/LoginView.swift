0faef120876cd0cd36482e4d78689c00b21e98b2
Jeong Dong <eejxdong99@gmail.com>
2024-01-05 18:39:45 +0900

    L: LoginView

---
+++ MessengerApp/LMessenger/View/Login/LoginView.swift
@@ -0,0 +1,57 @@
+//
+//  SwiftUIView.swift
+//  LMessenger
+//
+//  Created by 이정동 on 1/5/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Group {
+                Text("로그인")
+                    .font(.system(size: 28, weight: .bold))
+                    .foregroundStyle(Color.bkText)
+                    .padding(.top, 80)
+                
+                Text("아래 제공되는 서비스로 로그인을 해주세요.")
+                    .font(.system(size: 14))
+                    .foregroundStyle(Color.greyDeep)
+            }
+            .padding(.horizontal, 30)
+            
+            Spacer()
+            
+            Button {
+                // TODO: google login
+            } label: {
+                Text("Google로 로그인")
+            }
+            .buttonStyle(LoginButtonStyle(textColor: .bkText, borderColor: .greyLight))
+            
+            Button {
+                // TODO: apple login
+            } label: {
+                Text("Apple로 로그인")
+            }
+            .buttonStyle(LoginButtonStyle(textColor: .bkText, borderColor: .greyLight))
+        }
+        .toolbar {
+            ToolbarItemGroup(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image("back")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
