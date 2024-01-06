249214e4c82827ae913e82f4558d55f5b0288296
Jeong Dong <eejxdong99@gmail.com>
2024-01-06 19:27:02 +0900

    C: Splash View

---
+++ CommerceApp/CCommerce/SplashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewController.swift
+//  CCommerce
+//
+//  Created by 이정동 on 1/6/24.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
